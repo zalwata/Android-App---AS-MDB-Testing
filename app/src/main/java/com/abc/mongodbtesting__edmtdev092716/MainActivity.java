75c9f4e95d84b0bf738d808327d88d72d656383d
zalwata <zalwata17@gmail.com>
2020-06-02 20:44:37 -0700

    initial set up

---
+++ app/src/main/java/com/abc/mongodbtesting__edmtdev092716/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abc.mongodbtesting__edmtdev092716;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
